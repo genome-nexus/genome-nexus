984b9b7a5ee835f4b9bb3737bddf0f1079218763
Onur Sumer <s.onur.sumer@gmail.com>
2016-03-18 12:43:26 -0400

    returning empty list instead of null for unknown sources

--- annotation/src/main/java/org/cbioportal/genome_nexus/annotation/service/internal/VEPIsoformOverrideService.java
+++ annotation/src/main/java/org/cbioportal/genome_nexus/annotation/service/internal/VEPIsoformOverrideService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -50,7 +51,7 @@ public class VEPIsoformOverrideService implements IsoformOverrideService
         }
         else
         {
-            return null;
+            return Collections.emptyList();
         }
     }
 }
