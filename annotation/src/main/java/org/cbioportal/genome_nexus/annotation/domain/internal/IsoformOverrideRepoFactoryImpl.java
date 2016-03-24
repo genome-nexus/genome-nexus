05c6d049b8864300e93bfd2b5c17f6c2a764160f
Onur Sumer <s.onur.sumer@gmail.com>
2016-03-24 17:05:51 -0400

    updated one of the delimiters for the override URIs property value

--- annotation/src/main/java/org/cbioportal/genome_nexus/annotation/domain/internal/IsoformOverrideRepoFactoryImpl.java
+++ annotation/src/main/java/org/cbioportal/genome_nexus/annotation/domain/internal/IsoformOverrideRepoFactoryImpl.java
@@ -46,7 +46,7 @@ public class IsoformOverrideRepoFactoryImpl implements IsoformOverrideRepoFactor
 
         for (String pair: overrideURIs.split(","))
         {
-            String parts[] = pair.split(":");
+            String parts[] = pair.split("=");
 
             if (parts.length > 1)
             {
