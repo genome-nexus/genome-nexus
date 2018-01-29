72f8c66de5b826e3a8b760c885e22880429d23e0
dionnezaal <dionnezaal@gmail.com>
2018-01-29 12:02:41 +0100

    Scripts to transform ENSEMBL gff3 file to exon information file and add information (together with the domains) to the transcript json file

---
+++ scripts/add_domains_and_exon_information_to_ensembl_transcript.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import sys
+from collections import defaultdict
+
+if __name__ == "__main__":
+    # Read in transcript information file
+    transcripts = pd.read_csv("../data/ensembl_biomart_transcripts.txt", sep='\t')
+
+    ## Domains
+    domains_file = "../data/ensembl_biomart_pfam_grch37.p13.txt"
+    # Make a dictionary where every value added will be an empty list
+    # With this dictionary no keys have to be added for transcripts without domains
+    # This will automatically be empty list when transcript id (key in dict) is requested
+    domain_dict = defaultdict(list)
+
+    # Loop through each line of the file with pfam domain info and append information
+    # to dictionary, which has transcript id as key
+    with open(domains_file, 'r') as domains:
+        for line in domains:
+            # Split line and remove enter from last value
+            list_line = line.split("\t")
+            list_line[6] = list_line[6].replace("\n", "")
+            # Append information to dict, ensure correct mapping in json later
+            # not necessary to check if transcript id is in transcript file,
+            # will be done when dictionary is added to the correct rows of the transcript file
+            line_dict = {}
+            line_dict['pfam_domain_id'] = list_line[4]
+            line_dict['pfam_domain_start'] = list_line[5]
+            line_dict['pfam_domain_end'] = list_line[6]
+            # Append to dictionary with transcript ids
+            # A transcript can have multiple domains,
+            # therefore each domain will be appended to a list
+            domain_dict[list_line[1]].append(line_dict)
+
+    ## Exons
+    exon_file = "../data/ensembl_exon_info.txt"
+    # Also create dictionary where transcript ids will become the key
+    # and value is a empty list, unless it is filled by information from file
+    exon_dict = defaultdict(list)
+
+    # Loop through each line of the file with exon information, append information about
+    # exons to dictionary which have transcript id as key
+    with open(exon_file, 'r') as exons:
+        for line in exons:
+            # Split line to retrieve information and
+            list_line = line.split("\t")
+            list_line[6] = list_line[6].replace("\n", "")
+            # Add information to dictionary, not necessary to check if
+            # transcript id is in transcript file, will be done when dictionary
+            # is added to the correct rows of the transcript file
+            line_dict = {}
+            line_dict['exonId'] = list_line[1]
+            line_dict['exonStart'] = list_line[2]
+            line_dict['exonEnd'] = list_line[3]
+            line_dict['rank'] = list_line[4]
+            line_dict['strand'] = list_line[5]
+            line_dict['version'] = list_line[6]
+            # Add information to list in dictionary, because transcript can have
+            # multiple exons
+            exon_dict[list_line[0]].append(line_dict)
+
+    # Add dictionary values to correct row in transcript file
+    rows_domain = []
+    rows_exons = []
+    # Get transcript ids in order to append to correct row
+    transcript_ids = transcripts['transcript_stable_id'].tolist()
+    for row in transcript_ids:
+        rows_domain.append(domain_dict[row])
+        rows_exons.append(exon_dict[row])
+
+    # Add to dataframe and write to stdout
+    transcripts["domains"] = rows_domain
+    transcripts["exons"] = rows_exons
+    transcripts.to_json(sys.stdout, orient='records', lines=True)
+
+
---
+++ scripts/transform_gff_to_tsv_for_exon_info_from_ensembl.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import numpy as np
+import sys
+
+def transform_gff_to_tsv(gff_file):
+
+    # Dataframe to append the exon information
+    exon_info = pd.DataFrame(columns=['transcriptId', 'exonId', 'exonStart', 'exonEnd', 'rank', 'strand', 'version'])
+    # list to append exon information per row
+    rows = []
+
+    # Open gff file and read lines, when line contains exon information, extract this information
+    with open(gff_file, 'r') as gff:
+        for line in gff:
+            list_line = line.split("\t")
+            if len(list_line) > 1 and "exon" in list_line[2]:
+                data_dict = {}
+                line_exon_info = list_line[8].split(";")
+
+                try:
+                    data_dict['transcriptId'] = line_exon_info[0].split(":")[1]
+                except IndexError:
+                    data_dict['transcriptId'] = np.NaN
+
+                try:
+                    data_dict['exonId'] = line_exon_info[5].split("=")[1]
+                except IndexError:
+                    data_dict['exonId'] = np.NaN
+
+                try:
+                    data_dict['exonStart'] = list_line[3]
+                except IndexError:
+                    data_dict['exonStart'] = np.NaN
+
+                try:
+                    data_dict['exonEnd'] = list_line[4]
+                except IndexError:
+                    data_dict['exonEnd'] = np.NaN
+
+                try:
+                    data_dict['rank'] = line_exon_info[6].split("=")[1]
+                except IndexError:
+                    data_dict['rank'] = np.NaN
+
+                try:
+                    strand = list_line[6]
+                    # Convert plus strand into 1 and minus strand into -1
+                    if strand == "+":
+                        data_dict['strand'] = 1
+                    elif strand == "-":
+                        data_dict['strand'] = -1
+                    else:
+                        data_dict['strand'] = np.NaN
+                except IndexError:
+                    data_dict['strand'] = np.NaN
+
+                try:
+                    data_dict['version'] = line_exon_info[7].split("=")[1].replace("\n","")
+                except IndexError:
+                    data_dict['version'] = np.NaN
+
+                rows.append(data_dict)
+
+    print(rows[0:5])
+    print(len(rows))
+    exon_info = exon_info.append(rows, ignore_index=True)
+    return(exon_info)
+
+def main():
+    tsv_exon_info = transform_gff_to_tsv("../data/Homo_sapiens.GRCh37.87.gff3")
+    tsv_exon_info.to_csv(sys.stdout, sep='\t', index=False)
+main()
