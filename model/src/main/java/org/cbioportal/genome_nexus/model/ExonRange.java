ae3a24588ca13dac5485070908cad7ed0558e0ab
Ino de Bruijn <ino@ino.pm>
2018-02-15 16:06:55 -0500

    Add exon information to Ensembl transcripts
    Add exon information to the current EnsemblTranscript model, called
    ExonRange:
    
    ```
    ExonRange {
        exonId (string): Exon id,
        exonStart (integer): Start position of exon,
        exonEnd (integer): End position of exon ,
        rank (integer): Number of exon in transcript ,
        strand (integer): Strand exon is on, -1 for - and 1 for +,
        version (integer): Exon version
    }
    ```
    
    - Add model ExonRange to EnsemblTranscript Model
    
    Co-authored-by: dionnezaal <dionnezaal@gmail.com>

---
+++ model/src/main/java/org/cbioportal/genome_nexus/model/ExonRange.java
@@ -0,0 +1,72 @@
+package org.cbioportal.genome_nexus.model;
+
+import org.springframework.data.mongodb.core.mapping.Field;
+
+public class ExonRange
+{
+    @Field(value="exonId")
+    private String exonId;
+
+    @Field(value="exonStart")
+    private Integer exonStart;
+
+    @Field(value="exonEnd")
+    private Integer exonEnd;
+
+    @Field(value="rank")
+    private Integer rank;
+
+    @Field(value="strand")
+    private Integer strand;
+
+    @Field(value="version")
+    private Integer version;
+
+    public String getExonId() {
+        return this.exonId;
+    }
+
+    public void setExonId(String exonId) {
+        this.exonId = exonId;
+    }
+
+    public Integer getExonStart() {
+        return this.exonStart;
+    }
+
+    public void setExonStart(Integer exonStart) {
+        this.exonStart = exonStart;
+    }
+
+    public Integer getExonEnd() {
+        return this.exonEnd;
+    }
+
+    public void setExonEnd(Integer exonEnd) {
+        this.exonStart = exonEnd;
+    }
+    
+        public Integer getRank() {
+        return this.rank;
+    }
+
+    public void setRank(Integer rank) {
+        this.rank = rank;
+    }
+
+    public Integer getStrand() {
+        return this.strand;
+    }
+
+    public void setStrand(Integer strand) {
+        this.strand = strand;
+    }
+
+    public Integer getVersion() {
+        return this.version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+}
---
+++ web/src/main/java/org/cbioportal/genome_nexus/web/mixin/ExonRangeMixin.java
@@ -0,0 +1,25 @@
+package org.cbioportal.genome_nexus.web.mixin;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModelProperty;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ExonRangeMixin {
+    @ApiModelProperty(value = "Exon id", position=1, required = true)
+    private String exonId;
+
+    @ApiModelProperty(value = "Start position of exon", position=2, required = true)
+    private String exonStart;
+
+    @ApiModelProperty(value = "End position of exon", position=3, required = true)
+    private String exonEnd;
+
+    @ApiModelProperty(value = "Number of exon in transcript", position=4, required = true)
+    private String rank;
+
+    @ApiModelProperty(value = "Strand exon is on, -1 for - and 1 for +", position=5, required = true)
+    private String strand;
+
+    @ApiModelProperty(value = "Exon version", position=6, required = true)
+    private String version;
+}
