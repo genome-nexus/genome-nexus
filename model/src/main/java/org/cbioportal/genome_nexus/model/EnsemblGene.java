7b24d963f42a024d1966e18ce0f5e192c233b62d
Ino de Bruijn <ino@ino.pm>
2018-05-09 13:34:05 -0400

    Hugo to gene id endpoint fixes
    - Query in order: 1. approved, 2. previous symbols, 3. synonyms. This is
      necessary because there is overlap between these three.
    - Show hugo symbol in response

--- model/src/main/java/org/cbioportal/genome_nexus/model/EnsemblGene.java
+++ model/src/main/java/org/cbioportal/genome_nexus/model/EnsemblGene.java
@@ -6,18 +6,24 @@ package org.cbioportal.genome_nexus.model;
 public class EnsemblGene
 {
     private String geneId;
+    private String hugoSymbol;
     private String[] synonyms;
     private String[] previousSymbols;
 
     public EnsemblGene(EnsemblCanonical cn)
     {
         this.geneId = cn.getEnsemblCanonicalGeneId();
+        this.hugoSymbol = cn.getHugoSymbol();
         this.synonyms = cn.getSynonyms();
         this.previousSymbols = cn.getPreviousSymbols();
     }
 
     public String getGeneId() {
         return this.geneId;
+    }
+
+    public String getHugoSymbol() {
+        return this.hugoSymbol;
     }
 
     public String[] getSynonyms() {
