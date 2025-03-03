ff3c455f758998596e1d481e369577cec4d1e4d5
bprize15 <148106517+bprize15@users.noreply.github.com>
2025-03-03 16:36:58 -0500

    only save hgvsg as keys in index db (#786)
    * only save hgvsg as keys in index db
    
    * fix failing test
    
    * fix rebase error

--- service/src/main/java/org/cbioportal/genome_nexus/service/internal/VariantAnnotationService.java
+++ service/src/main/java/org/cbioportal/genome_nexus/service/internal/VariantAnnotationService.java
@@ -192,7 +192,7 @@ public class VariantAnnotationService
             // if caching is enabled, add to the index DB 
             if (cacheEnabled) {
                 variantAnnotation.ifPresent(annotation -> {
-                    this.saveToIndexDb(normalizedVariant, annotation);
+                    this.saveToIndexDb(annotation);
                 });
             }
 
@@ -251,7 +251,7 @@ public class VariantAnnotationService
                     .findFirst();
                 if (normalizedToOriginal.isPresent()) {
                     if (cacheEnabled) {
-                        this.saveToIndexDb(normalizedToOriginal.get()[0], variantAnnotation);
+                        this.saveToIndexDb(variantAnnotation);
                     }
                     String originalVariantQuery = normalizedToOriginal.get()[1];
                     // Multiple variants may normalize to same HGVS, remove so isn't used multiple times
@@ -279,10 +279,12 @@ public class VariantAnnotationService
         return variantAnnotations;
     }
 
-    public void saveToIndexDb(String normalizedVariant, VariantAnnotation annotation) {
-        Gson gson = new Gson();
-        DBObject dbObject = BasicDBObject.parse(gson.toJson(this.buildIndex(annotation)));
-        this.indexRepository.saveDBObject("index", normalizedVariant, dbObject);
+    public void saveToIndexDb(VariantAnnotation annotation) {
+        if (annotation.getHgvsg() != null) {
+            Gson gson = new Gson();
+            DBObject dbObject = BasicDBObject.parse(gson.toJson(this.buildIndex(annotation)));
+            this.indexRepository.saveDBObject("index", annotation.getHgvsg(), dbObject);
+        }
     }
 
 
